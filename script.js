1f9c2a8e0340903a95076d31b55fedd7fd1ff1bd
Amadeus <github@amadeus.me.uk>
2017-05-01 09:59:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,17 @@ function success(position){
 	var userLat = position.coords.latitude;
 	var userLon = position.coords.longitude;
 
+	// Where is the target location?
+	var targetLat = 0;
+	var targetLon = 0;
+
+	// Calculate the distance away from the target
+
+	var distance = calculateDistance(userLat, userLon, targetLat, targetLon, "M");
+
+	// Define the radius for application action
+	var radius = 0; // in miles
+
 } // END success
 
 // When geolocation fails
